493646d0748a26fbf5af3f53218c3be0a5928fac
blu3fire89 <blu3fire89@gmail.com>
2025-01-16 21:27:03 +0800

    optimize mapbox

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -12,7 +12,7 @@ export default {
     // Initialize the map
     const map = new mapboxgl.Map({
       container: "map", // ID of the HTML element to contain the map
-      style: "mapbox://styles/mapbox/streets-v11", // Map style
+      style: "mapbox://styles/mapbox/outdoors-v12?optimize=true", // Map style
       center: [119.96682240258258, 15.34890682307273], // Initial map center [longitude, latitude]
       zoom: 14, // Initial map zoom level
     });
@@ -33,5 +33,12 @@ export default {
   width: 100%;
   height: 400px; /* Adjust height as needed */
   margin-top: 20px;
+}
+#static {
+  background-image: url(`https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/119.966,15.348,14/600x400?access_token={{import.meta.env.VITE_MAP_TOKEN}}`);
+  width: 100%;
+  height: 100%;
+  grid-column: 1;
+  grid-row: 1;
 }
 </style>
